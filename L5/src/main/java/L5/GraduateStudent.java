177d79c552c90cd6c4e69859a509a9395662cefa
Rehan <rehannags@yahoo.com>
2021-01-06 15:01:51 +0400

    initial commit

---
+++ L5/src/main/java/L5/GraduateStudent.java
@@ -0,0 +1,73 @@
+package L5;
+
+public class GraduateStudent extends Student{
+    private String supervisor;
+    private boolean isPhD;
+    private String undergraduateSchool;
+
+    public GraduateStudent(String Lastname, String program, String year) throws Exception {
+        super(Lastname, program, year);
+        isPhD = false;
+    }
+
+    public GraduateStudent(String Lastname, String program, String year, double grade, String supervisor, boolean isPhD, String undergraduateSchool) throws Exception {
+        super(Lastname, program, year, grade);
+        this.supervisor = supervisor;
+        this.isPhD = false;
+        this.isPhD = isPhD;
+        this.undergraduateSchool = undergraduateSchool;
+    }
+
+    public void setSupervisor(String supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public void setIsPhD(String isPhD) {
+        this.isPhD = Boolean.parseBoolean(isPhD);
+    }
+
+    public void setUndergraduateSchool(String UndergraduateSchool) {
+        this.undergraduateSchool = UndergraduateSchool;
+    }
+
+    public String getSupervisor() {
+        return supervisor;
+    }
+    
+    public boolean getIsPhD() {
+        return isPhD;
+    }
+
+    public String getUndergraduateSchool() {
+        return undergraduateSchool;
+    }
+
+    @Override
+    public String toString() {
+        String tempString = super.toString();
+        if (isPhD) {
+            tempString = tempString + "PhD";
+        } else {
+            tempString = tempString + "Masters";
+        }
+        tempString = tempString + "\nSupervisor: " + this.getSupervisor() + 
+                    "\nUndergraduate School: " + this.getUndergraduateSchool() + "\n";
+        return tempString;
+    }
+
+    public String toStringInLine() {
+        String tempString = super.toStringInLine();
+        tempString = tempString + " " + this.supervisor + " ";
+        if (this.getIsPhD() == true) {
+            tempString = tempString + "1 ";
+        } else {
+            tempString = tempString +"0 ";
+        }
+        return (tempString + this.getUndergraduateSchool());
+    }
+
+    @Override
+    public boolean equals (Object tempObject) {
+        return this == tempObject;
+    }
+}
---
+++ L5/src/main/java/L5/Student.java
@@ -0,0 +1,92 @@
+package L5;
+
+public class Student {
+    private String lastName;
+    private String program;
+    private int year;
+    private double averageGrade = 0.0;
+
+    public Student(String Lastname, String program, String year) throws Exception {
+        this.lastName = Lastname;
+        this.program = program;
+        try {
+            this.year = Integer.parseInt(year);
+        } catch (Exception e) {
+            //System.out.println("Error: the year argument cannot be turned to an integer");
+            throw new Exception("error could not convert year to integer");
+        }
+        averageGrade = 0.0;
+    }
+
+    public Student(String Lastname, String program, String year, double grade) throws Exception {
+        this.lastName = Lastname;
+        this.program = program;
+        try {
+            this.year = Integer.parseInt(year);
+        } catch (Exception e) {
+            //System.out.println("Error: the year argument cannot be turned to an integer");
+            throw new Exception("error could not convert year to integer");
+        }
+        if (grade>=0 && grade<=100) {
+            averageGrade = grade;
+        } else {
+            throw new Exception("Fatal error: average grade not in range of 0-100");
+        }
+        
+    }
+
+    public void setGrade(double grd) {
+        if (grd> 100 || grd < 0) {
+            System.out.println("Error: the grade should be between 0 and 100 (inclusive)");
+        } else {
+            this.averageGrade = grd;
+        }
+    }
+
+    public double getGrade() {
+        return averageGrade;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return "Last Name: " + this.getLastName() +
+               "\nProgram: " + this.getProgram() +
+               "\nYear: " + this.getYear() +
+               "\nAverage Grade: " + this.getGrade() + "\n";
+    }
+
+    public String toStringInLine() {
+        return (this.getLastName() + " " + this.getProgram() + " " + this.getYear() + " " + this.getGrade());
+    }
+
+    public boolean isEqual(String str1) {
+        return program.equals(str1);
+    }
+
+    public boolean equals (Object tempObject) {
+        return this == tempObject;
+    }
+}
